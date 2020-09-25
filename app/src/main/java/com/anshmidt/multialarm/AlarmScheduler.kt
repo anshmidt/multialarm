ecf9affa7d9ac5f364da1e42de7c19d3879cbd56
anshmidt <anshmidt_ilya@list.ru>
2020-09-25 23:13:38 +0300

    Added method for alarm scheduling

--- app/src/main/java/com/anshmidt/multialarm/AlarmScheduler.kt
+++ app/src/main/java/com/anshmidt/multialarm/AlarmScheduler.kt
@@ -4,12 +4,21 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import com.anshmidt.multialarm.data.AlarmSettings
+import com.anshmidt.multialarm.data.TimeFormatter
+import java.util.concurrent.TimeUnit
 
 class AlarmScheduler(val context: Context) {
 
     private val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
-    fun schedule(firstAlarmTimeMillis: Long, intervalBetweenAlarmsMillis: Long) {
+    fun schedule(alarmSettings: AlarmSettings) {
+        val firstAlarmTimeMillis = TimeFormatter.getFirstAlarmTimeMillis(alarmSettings.firstAlarmTime)
+        val intervalBetweenAlarmsMillis = TimeUnit.MINUTES.toMillis(alarmSettings.minutesBetweenAlarms.toLong())
+        schedule(firstAlarmTimeMillis = firstAlarmTimeMillis, intervalBetweenAlarmsMillis = intervalBetweenAlarmsMillis)
+    }
+
+    private fun schedule(firstAlarmTimeMillis: Long, intervalBetweenAlarmsMillis: Long) {
         val alarmIntent = getAlarmIntent()
         val alarmType = AlarmManager.RTC_WAKEUP
         alarmManager.setRepeating(
