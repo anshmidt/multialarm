bce53365440dabd653ca3c7b4943ee7e8fd932de
anshmidt <anshmidt_ilya@list.ru>
2022-08-29 16:33:20 +0200

    Added splash screen

---
+++ app/src/main/java/com/anshmidt/multialarm/view/activities/SplashScreenActivity.kt
@@ -0,0 +1,16 @@
+package com.anshmidt.multialarm.view.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+        finish()
+    }
+}
--- app/src/main/java/com/anshmidt/multialarm/view/helpers/AppThemeSelector.kt
+++ app/src/main/java/com/anshmidt/multialarm/view/helpers/AppThemeSelector.kt
@@ -2,23 +2,22 @@ package com.anshmidt.multialarm.view.helpers
 
 import androidx.appcompat.app.AppCompatDelegate
 import com.anshmidt.multialarm.repository.IAppSettingRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
 
 class AppThemeSelector(
         val appSettingRepository: IAppSettingRepository
 ) {
 
     fun checkAndShowTheme() {
-        CoroutineScope(SupervisorJob()).launch(Dispatchers.Main) {
+//        CoroutineScope(SupervisorJob()).launch(Dispatchers.Main) {
+        runBlocking {
             appSettingRepository.getNightModeSwitchState().first { nightModeSwitchState ->
                 selectAppTheme(nightModeSwitchState)
                 return@first true
             }
         }
+//        }
     }
 
     private fun selectAppTheme(isNightModeOn: Boolean) {
