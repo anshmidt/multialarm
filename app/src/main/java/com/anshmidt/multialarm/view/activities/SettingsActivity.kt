2e7c045bfea881eb74888b7f0b359106740580ec
anshmidt <anshmidt_ilya@list.ru>
2022-08-29 21:05:04 +0200

    Fixed transition animation on Settings screen

--- app/src/main/java/com/anshmidt/multialarm/view/activities/SettingsActivity.kt
+++ app/src/main/java/com/anshmidt/multialarm/view/activities/SettingsActivity.kt
@@ -1,9 +1,11 @@
 package com.anshmidt.multialarm.view.activities
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.anshmidt.multialarm.R
 import com.anshmidt.multialarm.view.fragments.SettingsFragment
+
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +17,18 @@ class SettingsActivity : AppCompatActivity() {
                 .replace(R.id.framelayout_settings, SettingsFragment())
                 .commit()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    /**
+     * Overriding pressing Up button in order to change transition animation
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            android.R.id.home -> {
+                finish()
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
